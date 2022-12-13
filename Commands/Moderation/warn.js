7f076ee7ab1a82e8f4ac74871f423d9adb046c3c
killerclaws12 <78247679+killerclaws12@users.noreply.github.com>
2022-12-12 22:52:00 -0500

    Warning command

---
+++ Commands/Moderation/warn.js
@@ -0,0 +1,138 @@
+const { Client, ChatInputCommandInteraction, EmbedBuilder } = require("discord.js")
+const DB = require("../../Structures/Schemas/Warnings")
+const Reply = require("../../Systems/Reply")
+const EditReply = require("../../Systems/EditReply")
+
+module.exports = {
+    name: "warn",
+    description: "Warns a member in the server.",
+    UserPerms: ["ManageGuild"],
+    category: "Moderation",
+    options: [
+        {
+            name: 'add',
+            description: "Adds a warn to a member",
+            type: 1,
+            options: [
+                {
+                    name: "user",
+                    description: "Select the user",
+                    type: 6,
+                    required: true
+                },
+                {
+                    name: "reason",
+                    description: "Provide a reason",
+                    type: 3,
+                    required: false
+                }
+            ]
+        },
+        {
+            name: 'remove',
+            description: "Removes a warn to a member",
+            type: 3,
+            options: [
+                {
+                    name: "warn-id",
+                    description: "Enter the warn ID",
+                    type: 6,
+                    required: true
+                },
+            ],
+        },
+        {
+            name: 'list',
+            description: "Displays the list of warns a member has",
+            type: 1,
+            options: [
+                {
+                    name: "user",
+                    description: "Select a user",
+                    type: 6,
+                    required: true
+                },
+            ],
+        }
+    ],
+
+    async execute(interaction, client) {
+
+        const { options, user, guild } = interaction
+
+        switch (options.getSubcommand()) {
+
+            case "add": {
+                const Target = options.getMember("user")
+                const Reason = options.getString("reason") || "No reason provided"
+
+                if (Target.id === user.id) return Reply(interaction, "❌", `You can't warn yourself!`)
+                if (guild.ownerId === Target.id) return Reply(interaction, "❌", `You can't warn the server owner!`)
+
+                await interaction.deferReply()
+
+                let Data = new DB({
+                    User: Target.id,
+                    Guild: guild.id,
+                    Moderator: user.id,
+                    Reason: Reason,
+                    Timestamp: Date.now()
+                })
+
+                await Data.save()
+
+                EditReply(interaction, "✅", `${Target} has been warned for: ${Reason}`)
+
+            }
+
+                break;
+
+            case "remove": {
+
+                await interaction.deferReply()
+
+                const WarnID = options.getString("warn-id")
+
+                const Data = await DB.findOne({ _id: WarnID }).catch(err => { })
+                if (!Data) return EditReply(interaction, "❌", `The ID you provided is not valid!`)
+
+                const Member = guild.members.cache.get(Data.User)
+
+                await Data.delete()
+
+                EditReply(interaction, "✅", `Removed warning from ${Member}`)
+
+            }
+
+                break;
+
+            case "list": {
+
+                await interaction.deferReply()
+
+                const Target = options.getMember("user")
+                if (!Target) return EditReply(interaction, "❌", `The member doesn't exist!`)
+
+                const UserWarns = await DB.find({ User: Target.id, Guild: guild.id }).catch(err => { })
+                if (UserWarns.length === 0) return EditReply(interaction, "❌", `Member doesn't have any warnings!`)
+
+                const Mapped = UserWarns.map(warn => {
+
+                    return `\`${warn._id}\` | ${warn.Reason}`
+
+                }).join("\n")
+
+                const Embed = new EmbedBuilder()
+                    .setColor(client.color)
+                    .setDescription(`${Mapped}`)
+
+                interaction.editReply({ embeds: [Embed]})
+
+            }
+
+                break;
+
+        }
+
+    }
+}
