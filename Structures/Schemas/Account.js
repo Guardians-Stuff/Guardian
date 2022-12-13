181fb0afe0e8bca3ff40ca29437d7c734ea1b83d
killerclaws12 <78247679+killerclaws12@users.noreply.github.com>
2022-12-12 22:52:25 -0500

    feat: Added economy schemas

---
+++ Structures/Schemas/Account.js
@@ -0,0 +1,9 @@
+const { model, Schema } = require("mongoose")
+
+module.exports = model("account", new Schema({
+
+    User: String,
+    Bank: Number,
+    Wallet: Number
+
+}))
---
+++ Structures/Schemas/MoneyActions.js
@@ -0,0 +1,8 @@
+const { model, Schema } = require("mongoose")
+
+module.exports = model("money-action", new Schema({
+
+    User: String,
+    Daily: Number
+
+}))
