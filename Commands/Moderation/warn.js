08b0bd47fa51c8a9028d1e2f30f2ebc68cb3845c
killerclaws12 <78247679+killerclaws12@users.noreply.github.com>
2022-12-13 21:03:58 -0500

    fix: fixed warn.js

--- Commands/Moderation/warn.js
+++ Commands/Moderation/warn.js
@@ -1,49 +1,50 @@
 const { Client, ChatInputCommandInteraction, EmbedBuilder } = require("discord.js")
 const DB = require("../../Structures/Schemas/Warnings")
-const Reply = require("../../Systems/Reply")
 const EditReply = require("../../Systems/EditReply")
+const Reply = require("../../Systems/Reply")
+
 
 module.exports = {
     name: "warn",
-    description: "Warns a member in the server.",
+    description: "Add, remove, or check warns of a user in the guild.",
     UserPerms: ["ManageGuild"],
     category: "Moderation",
     options: [
         {
-            name: 'add',
-            description: "Adds a warn to a member",
+            name: "add",
+            description: "Adds a warning to a member",
             type: 1,
             options: [
                 {
                     name: "user",
-                    description: "Select the user",
+                    description: "Select a user",
                     type: 6,
                     required: true
                 },
                 {
                     name: "reason",
-                    description: "Provide a reason",
+                    description: "Provide a valid reason",
                     type: 3,
                     required: false
                 }
             ]
         },
         {
-            name: 'remove',
-            description: "Removes a warn to a member",
-            type: 3,
+            name: "remove",
+            description: "Removes a warning from a member",
+            type: 1,
             options: [
                 {
                     name: "warn-id",
-                    description: "Enter the warn ID",
-                    type: 6,
+                    description: "Provide the user warn ID",
+                    type: 3,
                     required: true
                 },
             ],
         },
         {
-            name: 'list',
-            description: "Displays the list of warns a member has",
+            name: "list",
+            description: "Displays all warns of a member",
             type: 1,
             options: [
                 {
@@ -61,16 +62,15 @@ module.exports = {
         const { options, user, guild } = interaction
 
         switch (options.getSubcommand()) {
-
             case "add": {
                 const Target = options.getMember("user")
                 const Reason = options.getString("reason") || "No reason provided"
 
-                if (Target.id === user.id) return Reply(interaction, "❌", `You can't warn yourself!`)
-                if (guild.ownerId === Target.id) return Reply(interaction, "❌", `You can't warn the server owner!`)
+                if (Target.id === user.id) return Reply(interaction, "❌", "You cannot warn yourself!")
+                if (guild.ownerId === Target.id) return Reply(interaction, "❌", "You cannot warn the server/guild owner!")
 
                 await interaction.deferReply()
-
+                // Creating a new document in the Database //
                 let Data = new DB({
                     User: Target.id,
                     Guild: guild.id,
@@ -78,61 +78,53 @@ module.exports = {
                     Reason: Reason,
                     Timestamp: Date.now()
                 })
-
                 await Data.save()
+                /////////////////////////////////////////////
 
-                EditReply(interaction, "✅", `${Target} has been warned for: ${Reason}`)
-
+                EditReply(interaction, `✅`, `**${Target}** has been warned for **${Reason}**`)
             }
 
                 break;
 
             case "remove": {
-
                 await interaction.deferReply()
 
                 const WarnID = options.getString("warn-id")
 
                 const Data = await DB.findOne({ _id: WarnID }).catch(err => { })
-                if (!Data) return EditReply(interaction, "❌", `The ID you provided is not valid!`)
+                if (!Data) return EditReply(interaction, "❌", "The ID you provide is not valid.")
 
                 const Member = guild.members.cache.get(Data.User)
 
                 await Data.delete()
 
-                EditReply(interaction, "✅", `Removed warning from ${Member}`)
-
+                EditReply(interaction, `✅`, `Removed **warning** from **${Member}**!`)
             }
 
                 break;
 
             case "list": {
-
                 await interaction.deferReply()
 
                 const Target = options.getMember("user")
                 if (!Target) return EditReply(interaction, "❌", `The member doesn't exist!`)
 
                 const UserWarns = await DB.find({ User: Target.id, Guild: guild.id }).catch(err => { })
-                if (UserWarns.length === 0) return EditReply(interaction, "❌", `Member doesn't have any warnings!`)
+                if (UserWarns.length === 0) return Reply(interaction, "❌", `The member doesn't have any warnings.`)
 
                 const Mapped = UserWarns.map(warn => {
-
                     return `\`${warn._id}\` | ${warn.Reason}`
-
                 }).join("\n")
 
                 const Embed = new EmbedBuilder()
                     .setColor(client.color)
                     .setDescription(`${Mapped}`)
 
-                interaction.editReply({ embeds: [Embed]})
+                interaction.editReply({ embeds: [Embed] })
 
             }
 
                 break;
-
         }
-
     }
 }
