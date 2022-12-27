9ffc2afe6efbf473df0567a9a9ae806b928108ab
killerclaws12 <78247679+killerclaws12@users.noreply.github.com>
2022-12-26 21:25:55 -0500

    feat: userinfo

---
+++ Commands/Public/userinfo.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, User, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("userinfo")
+        .setDescription("Shows information about a user")
+        .addUserOption(option => option.setName("user").setDescription("The user to get information about")),
+    execute(interaction) {
+        const user = interaction.options.getUser("user") || interaction.user;
+        const userCreated = interaction?.guild?.members?.cache?.get(user.id)?.joinedTimestamp || user.createdTimestamp;
+
+        const embed = new EmbedBuilder()
+            .addFields([
+                { name: "👤 Username", value: `${user.username}#${user.discriminator}`, inline: true },
+                { name: "🆔 ID", value: user.id, inline: true },
+                { name: "📆 Created", value: `<t:${Math.floor(user.createdTimestamp / 1000)}:R>`, inline: true },
+                { name: "📆 Joined", value: `<t:${(Math.floor(userCreated / 1000))}:R>`, inline: true },
+                { name: "📥 Avatar", value: `[Click Here](${user.avatarURL()})`, inline: true },
+                { name: "🎮 Bot", value: user.bot ? "Yes" : "No", inline: true },
+                { name: "📷 Banner", value: user.bannerURL() ? `[Click Here](${user.bannerURL()})` : "None", inline: true },
+                { name: "🎭 System", value: user.system ? "Yes" : "No", inline: true },
+            ])
+            .setColor("#FFFFFF")
+            .setThumbnail(user.avatarURL())
+
+        interaction.reply({ embeds: [embed] });
+
+    },
+};
+
