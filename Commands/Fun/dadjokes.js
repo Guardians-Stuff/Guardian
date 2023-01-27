60a8cf51421d9007df510e9775b8df001bc20d6e
killerclaws12 <78247679+killerclaws12@users.noreply.github.com>
2023-01-27 15:45:09 -0500

    dadjokes command

---
+++ Commands/Fun/dadjokes.js
@@ -0,0 +1,16 @@
+const { PermissionFlagsBits, SlashCommandBuilder } = require("discord.js");
+const fs = require("fs");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("dadjoke")
+        .setDescription("Receive a random dad joke!"),
+    async execute(interaction) {
+        dadJokesTxt = fs.readFileSync("./data/dadjokes.txt");
+        dadJokesTxt = dadJokesTxt.toString();
+        dadJokesTxt = dadJokesTxt.split("\n");
+        randomNum = Math.floor(Math.random() * 710);
+
+        interaction.reply({ content: dadJokesTxt[randomNum], ephemeral: true });
+    },
+};
