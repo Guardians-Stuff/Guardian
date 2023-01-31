ef5628ffc4f872578e1f7cfc0f95f14535bd6cd2
Ryder <78247679+killerclaws12@users.noreply.github.com>
2023-01-30 19:10:31 -0500

    Update ping.js

--- Commands/Information/ping.js
+++ Commands/Information/ping.js
@@ -6,7 +6,7 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("ping")
         .setDMPermission(false)
-        .setDescription("Pong!"),
+        .setDescription("Ping pong!"),
     /**
      * 
      * @param {ChatInputCommandInteraction} interaction 
