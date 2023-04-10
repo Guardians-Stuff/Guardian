764a964ce3317fa0d3c8d9159361e8c1d88a1061
Killerclaws12 <hiltonbrennan70@gmail.com>
2023-04-10 04:07:25 -0400

    Updated inv

--- Commands/Information/invite.js
+++ Commands/Information/invite.js
@@ -1,17 +1,23 @@
-const { SlashCommandBuilder, ChatInputCommandInteraction, Client } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  ChatInputCommandInteraction,
+  Client,
+} = require("discord.js");
 
-const EmbedGenerator = require('../../Functions/embedGenerator');
+const EmbedGenerator = require("../../Functions/embedGenerator");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("invite")
-        .setDMPermission(false)
-        .setDescription("Sends an invite of the bot to the user"),
-    /**
-     * @param {ChatInputCommandInteraction} interaction 
-     * @param {Client} client
-     */
-    execute(interaction, client) {
-        return EmbedGenerator.basicEmbed(`[Click me for the invite to the bot!](https://discord.com/oauth2/authorize?client_id=1053736067129421884&scope=bot)`);
-    }
+  data: new SlashCommandBuilder()
+    .setName("invite")
+    .setDMPermission(false)
+    .setDescription("Sends an invite of the bot to the user"),
+  /**
+   * @param {ChatInputCommandInteraction} interaction
+   * @param {Client} client
+   */
+  execute(interaction, client) {
+    return EmbedGenerator.basicEmbed(
+      `[Click me for the invite to the bot!](https://discord.com/oauth2/authorize?client_id=1093047924172279900&scope=bot)`
+    );
+  },
 };
