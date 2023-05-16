bac31b8459a383f6d7f725bff0f7d7c9e2e32a6a
Mittelblut9 <sauer.benedikt@outlook.de>
2023-05-16 08:24:58 +0200

    fix/prettier changes

--- Commands/Moderator/audit.js
+++ Commands/Moderator/audit.js
@@ -9,56 +9,53 @@ module.exports = {
         .setDescription('Displays the audit log for the server')
         .setDefaultMemberPermissions(Discord.PermissionFlagsBits.ViewAuditLog)
         .addStringOption((option) =>
-            option
-                .setName('type')
-                .setDescription('The type of audit log to display')
-                .setChoices(
-                    { name: 'ban', value: Discord.AuditLogEvent.MemberBanAdd.toString() },
-                    { name: 'unban', value: Discord.AuditLogEvent.MemberBanRemove.toString() },
-                    { name: 'kick', value: Discord.AuditLogEvent.MemberKick.toString() },
-                    {
-                        name: 'message-delete',
-                        value: Discord.AuditLogEvent.MessageDelete.toString(),
-                    },
-                    {
-                        name: 'message-delete-bulk',
-                        value: Discord.AuditLogEvent.MessageBulkDelete.toString(),
-                    },
-                    { name: 'role-create', value: Discord.AuditLogEvent.RoleCreate.toString() },
-                    { name: 'role-delete', value: Discord.AuditLogEvent.RoleDelete.toString() },
-                    { name: 'role-update', value: Discord.AuditLogEvent.RoleUpdate.toString() },
-                    {
-                        name: 'channel-create',
-                        value: Discord.AuditLogEvent.ChannelCreate.toString(),
-                    },
-                    {
-                        name: 'channel-delete',
-                        value: Discord.AuditLogEvent.ChannelDelete.toString(),
-                    },
-                    {
-                        name: 'channel-update',
-                        value: Discord.AuditLogEvent.ChannelUpdate.toString(),
-                    },
-                    { name: 'emoji-create', value: Discord.AuditLogEvent.EmojiCreate.toString() },
-                    { name: 'emoji-delete', value: Discord.AuditLogEvent.EmojiDelete.toString() },
-                    { name: 'emoji-update', value: Discord.AuditLogEvent.EmojiUpdate.toString() },
-                    { name: 'invite-create', value: Discord.AuditLogEvent.InviteCreate.toString() },
-                    { name: 'invite-delete', value: Discord.AuditLogEvent.InviteDelete.toString() },
-                    {
-                        name: 'webhook-create',
-                        value: Discord.AuditLogEvent.WebhookCreate.toString(),
-                    },
-                    {
-                        name: 'webhook-delete',
-                        value: Discord.AuditLogEvent.WebhookDelete.toString(),
-                    },
-                    {
-                        name: 'webhook-update',
-                        value: Discord.AuditLogEvent.WebhookUpdate.toString(),
-                    },
-                    { name: 'member-update', value: Discord.AuditLogEvent.MemberUpdate.toString() },
-                    { name: 'member-move', value: Discord.AuditLogEvent.MemberMove.toString() }
-                )
+            option.setName('type').setDescription('The type of audit log to display').setChoices(
+                { name: 'ban', value: Discord.AuditLogEvent.MemberBanAdd.toString() },
+                { name: 'unban', value: Discord.AuditLogEvent.MemberBanRemove.toString() },
+                { name: 'kick', value: Discord.AuditLogEvent.MemberKick.toString() },
+                {
+                    name: 'message-delete',
+                    value: Discord.AuditLogEvent.MessageDelete.toString(),
+                },
+                {
+                    name: 'message-delete-bulk',
+                    value: Discord.AuditLogEvent.MessageBulkDelete.toString(),
+                },
+                { name: 'role-create', value: Discord.AuditLogEvent.RoleCreate.toString() },
+                { name: 'role-delete', value: Discord.AuditLogEvent.RoleDelete.toString() },
+                { name: 'role-update', value: Discord.AuditLogEvent.RoleUpdate.toString() },
+                {
+                    name: 'channel-create',
+                    value: Discord.AuditLogEvent.ChannelCreate.toString(),
+                },
+                {
+                    name: 'channel-delete',
+                    value: Discord.AuditLogEvent.ChannelDelete.toString(),
+                },
+                {
+                    name: 'channel-update',
+                    value: Discord.AuditLogEvent.ChannelUpdate.toString(),
+                },
+                { name: 'emoji-create', value: Discord.AuditLogEvent.EmojiCreate.toString() },
+                { name: 'emoji-delete', value: Discord.AuditLogEvent.EmojiDelete.toString() },
+                { name: 'emoji-update', value: Discord.AuditLogEvent.EmojiUpdate.toString() },
+                { name: 'invite-create', value: Discord.AuditLogEvent.InviteCreate.toString() },
+                { name: 'invite-delete', value: Discord.AuditLogEvent.InviteDelete.toString() },
+                {
+                    name: 'webhook-create',
+                    value: Discord.AuditLogEvent.WebhookCreate.toString(),
+                },
+                {
+                    name: 'webhook-delete',
+                    value: Discord.AuditLogEvent.WebhookDelete.toString(),
+                },
+                {
+                    name: 'webhook-update',
+                    value: Discord.AuditLogEvent.WebhookUpdate.toString(),
+                },
+                { name: 'member-update', value: Discord.AuditLogEvent.MemberUpdate.toString() },
+                { name: 'member-move', value: Discord.AuditLogEvent.MemberMove.toString() }
+            )
         )
         .addUserOption((option) =>
             option.setName('user').setDescription('The user to filter the audit log for')
--- Commands/Utility/djs.js
+++ Commands/Utility/djs.js
@@ -1,22 +1,17 @@
 const Discord = require(`discord.js`);
 
 module.exports = {
-  data: new Discord.SlashCommandBuilder()
-    .setName("docs")
-    .setDescription("Search the Discord.js documentation.")
-    .addStringOption((option) =>
-      option
-        .setName("query")
-        .setDescription("The search query.")
-        .setRequired(true)
-    ),
-  async execute(interaction) {
-    const query = interaction.options.getString("query");
-    const searchUrl = `https://discord.js.org/#/docs/main/stable/search?q=${encodeURIComponent(
-      query
-    )}`;
-    await interaction.reply(
-      `Here is the search result for "${query}": ${searchUrl}`
-    );
-  },
+    data: new Discord.SlashCommandBuilder()
+        .setName('docs')
+        .setDescription('Search the Discord.js documentation.')
+        .addStringOption((option) =>
+            option.setName('query').setDescription('The search query.').setRequired(true)
+        ),
+    async execute(interaction) {
+        const query = interaction.options.getString('query');
+        const searchUrl = `https://discord.js.org/#/docs/main/stable/search?q=${encodeURIComponent(
+            query
+        )}`;
+        await interaction.reply(`Here is the search result for "${query}": ${searchUrl}`);
+    },
 };
